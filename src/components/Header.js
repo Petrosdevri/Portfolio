f44595d444c22cadc052a22f0dbd15be26174b9b
Petros Devrikis <petrosdevri@protonmail.com>
2023-05-09 22:23:10 +0300

    Fixed overflow of Project Cards

--- src/components/Header.js
+++ src/components/Header.js
@@ -23,17 +23,3 @@ export default function Header() {
         </header>
     );
 }
-
- /* <nav className="navbar navbar-expand-lg bg-light">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="/"><img src={Logo} width='45' height='45' id='nav-logo' alt='nav-logo' /></a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item"><a className="nav-links" aria-current="page" href="#"><Home /> Home</a></li>
-                    </ul>
-                </div>
-            </div>
-            </nav> */
