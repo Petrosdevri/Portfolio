2b054464bc10fc85c7ec384e506e24b30e16f2c0
Petros Devrikis <petrosdevri@protonmail.com>
2023-05-10 23:44:12 +0300

    Improved width and restructured content

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -12,13 +12,13 @@ export default function Projects() {
     return (
         <div className='projects'>
             <Stack className='project-stack' direction='horizontal' gap={5}>
-                <ProjectCard title='ChadCoin' description='An ERC20 meme token with a frontend DApp.' picture={ChadCoin} code='https://github.com/Petrosdevri/ChadCoin' demo='https://github.com/Petrosdevri/ChadCoin#readme' />
-                <ProjectCard title='Frontend Mentor Challenges' description='My solutions to Frontend Mentor challenges.' picture={FrontendMentorChallenges} code='https://github.com/Petrosdevri/Frontend-Mentor-Challenges' demo='https://frontend-mentor-petrosdevri.netlify.app/' />
-                <ProjectCard title='Greek VAT Calculator' description='A web page to calculate VAT rates in Greece.' picture={GreekVATCalculator} code='https://github.com/Petrosdevri/Greek-VAT-Calculator' demo='https://greek-vat-calculator.vercel.app/' />
+                <ProjectCard title='ChadCoin' description='A Solidity ERC20 Meme Coin with a JavaScript frontend DApp.' picture={ChadCoin} code='https://github.com/Petrosdevri/ChadCoin' demo='https://github.com/Petrosdevri/ChadCoin#readme' />
+                <ProjectCard title='Frontend Mentor Challenges' description='My solutions to Frontend Mentor challenges, using HTML/CSS/JavaScript and Bootstrap.' picture={FrontendMentorChallenges} code='https://github.com/Petrosdevri/Frontend-Mentor-Challenges' demo='https://frontend-mentor-petrosdevri.netlify.app/' />
+                <ProjectCard title='Greek VAT Calculator' description='An HTML/CSS/JavaScript website to calculate VAT rates in Greece.' picture={GreekVATCalculator} code='https://github.com/Petrosdevri/Greek-VAT-Calculator' demo='https://greek-vat-calculator.vercel.app/' />
             </Stack>
             <Stack className='project-stack' direction='horizontal' gap={5}>
-                <ProjectCard title='Math Training Game' description='A training game developed to help people on math operations.' picture={MathTrainingGame} code='https://github.com/Petrosdevri/Math-Training-Game' demo='https://petrosdevri-math-game.netlify.app/' />
-                <ProjectCard title='Product CRUD App' description='A sample app that can accept product inputs to a list, built on Electron and MySQL.' picture='' code='https://github.com/Petrosdevri/Product-CRUD-App' demo='https://github.com/Petrosdevri/Product-CRUD-App' />
+                <ProjectCard title='Math Training Game' description='A training game built on HTML/CSS/JavaScript developed to help people on math operations.' picture={MathTrainingGame} code='https://github.com/Petrosdevri/Math-Training-Game' demo='https://petrosdevri-math-game.netlify.app/' />
+                <ProjectCard title='Product CRUD App' description='An Electron and MySQL sample app that can accept product inputs to a list, which is stored at a database.' picture='' code='https://github.com/Petrosdevri/Product-CRUD-App' demo='https://github.com/Petrosdevri/Product-CRUD-App' />
                 <ProjectCard title='React Quiz' description='A sample quiz built on React featuring questions about Greece.' picture={ReactQuiz} code='https://github.com/Petrosdevri/React-Greece-Quiz' demo='https://react-greece-quiz.netlify.app/' />
             </Stack>
         </div>
