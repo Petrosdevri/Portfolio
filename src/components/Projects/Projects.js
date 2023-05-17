442bc9af7be13fc6635617e053664cb3e32b7bd8
Petros Devrikis <petrosdevri@protonmail.com>
2023-05-17 19:10:21 +0300

    Added new projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,23 +2,29 @@ import React from 'react';
 import './Projects.css';
 import ProjectCard from './ProjectCard';
 import { Stack } from 'react-bootstrap';
-import ChadCoin from '../../images/photos/project-pictures/chadcoin.png';
-import FrontendMentorChallenges from '../../images/photos/project-pictures/frontend-mentor-challenges.png';
-import GreekVATCalculator from '../../images/photos/project-pictures/greek-vat-calculator.png';
-import MathTrainingGame from '../../images/photos/project-pictures/math-training-game.png';
-import ReactQuiz from '../../images/photos/project-pictures/react-quiz.png';
+import ChadCoin from '../../images/photos/project-pictures/ChadCoin.png';
+import FrontendMentorChallenges from '../../images/photos/project-pictures/Frontend Mentor Challenges.png';
+import GreekValues from '../../images/photos/project-pictures/GreekValues.png';
+import GreekVATCalculator from '../../images/photos/project-pictures/Greek VAT Calculator.png';
+import JasonPortfolio from '../../images/photos/project-pictures/Jason Portfolio.png'
+import MathTrainingGame from '../../images/photos/project-pictures/Math Training Game.png';
+import ReactQuiz from '../../images/photos/project-pictures/React Quiz.png';
 
 export default function Projects() {
     return (
         <div className='projects'>
             <Stack className='project-stack' direction='horizontal' gap={5}>
-                <ProjectCard title='ChadCoin' description='A Solidity ERC20 Meme Coin with a JavaScript frontend DApp.' picture={ChadCoin} code='https://github.com/Petrosdevri/ChadCoin' demo='https://github.com/Petrosdevri/ChadCoin#readme' />
+                <ProjectCard title='ChadCoin' description='A Solidity ERC20 Meme Coin with a JavaScript frontend DApp. Was done for Cadena Web3 course.' picture={ChadCoin} code='https://github.com/Petrosdevri/ChadCoin' demo='https://github.com/Petrosdevri/ChadCoin#readme' />
                 <ProjectCard title='Frontend Mentor Challenges' description='My solutions to Frontend Mentor challenges, using HTML/CSS/JavaScript and Bootstrap.' picture={FrontendMentorChallenges} code='https://github.com/Petrosdevri/Frontend-Mentor-Challenges' demo='https://frontend-mentor-petrosdevri.netlify.app/' />
-                <ProjectCard title='Greek VAT Calculator' description='An HTML/CSS/JavaScript website to calculate VAT rates in Greece.' picture={GreekVATCalculator} code='https://github.com/Petrosdevri/Greek-VAT-Calculator' demo='https://greek-vat-calculator.vercel.app/' />
+                <ProjectCard title='Greek Values' description='The ultimate Greek political quiz, GreekValues, tries to identify popular opinions with Greek ideologies and parties. Built on React.' picture={GreekValues} code='https://github.com/PoliVerse/GreekValues' demo='https://greek-values.vercel.app/' />
             </Stack>
             <Stack className='project-stack' direction='horizontal' gap={5}>
+                <ProjectCard title='Greek VAT Calculator' description='An HTML/CSS/JavaScript website in which users can calculate VAT rates in Greece by inserting their costs.' picture={GreekVATCalculator} code='https://github.com/Petrosdevri/Greek-VAT-Calculator' demo='https://greek-vat-calculator.vercel.app/' />
+                <ProjectCard title='Jason Portfolio' description='A single-page portfolio website built on React for a friend of mine.' picture={JasonPortfolio} code='https://github.com/Petrosdevri/JasonPortfolio' demo='https://jasonkehagias.netlify.app/' />
                 <ProjectCard title='Math Training Game' description='A training game built on HTML/CSS/JavaScript developed to help people on math operations.' picture={MathTrainingGame} code='https://github.com/Petrosdevri/Math-Training-Game' demo='https://petrosdevri-math-game.netlify.app/' />
-                <ProjectCard title='Product CRUD App' description='An Electron and MySQL sample app that can accept product inputs to a list, which is stored at a database.' picture='' code='https://github.com/Petrosdevri/Product-CRUD-App' demo='https://github.com/Petrosdevri/Product-CRUD-App' />
+            </Stack>
+            <Stack className='project-stack' direction='horizontal' gap={5}>
+                <ProjectCard title='Product CRUD App' description='An Electron and MySQL sample app. Users can submit inputs about a product which are stored at a database and displayed at the interface.' picture='' code='https://github.com/Petrosdevri/Product-CRUD-App' demo='https://github.com/Petrosdevri/Product-CRUD-App' />
                 <ProjectCard title='React Quiz' description='A sample quiz built on React featuring questions about Greece.' picture={ReactQuiz} code='https://github.com/Petrosdevri/React-Greece-Quiz' demo='https://react-greece-quiz.netlify.app/' />
             </Stack>
         </div>
