82bf0a3c64bbbb5203c3e34765905f8b8b6b289b
Petros Devrikis <petrosdevri@protonmail.com>
2023-05-19 18:28:30 +0300

    Updated footer.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import { React, lazy, useState } from 'react';
 import { Container, Stack } from 'react-bootstrap';
 import LazyLoad from 'react-lazyload';
-import { Sidevdotto as Devto, Sihashnode as Hashnode, Siprotonmail as ProtonMail } from '@icons-pack/react-simple-icons';
+import { Siprotonmail as ProtonMail } from '@icons-pack/react-simple-icons';
 const UilGithub = lazy(() => import('@iconscout/react-unicons/icons/uil-github'));
 const UilInstagram = lazy(() => import('@iconscout/react-unicons/icons/uil-instagram'));
 const UilLinkedin = lazy(() => import('@iconscout/react-unicons/icons/uil-linkedin'));
@@ -10,8 +10,8 @@ export default function Footer() {
     let date = new Date();
     let year = date.getFullYear();
 
-    const [color, setColor] = useState(['#fff', '#fff', '#fff', '#fff', '#fff', '#fff']);
-    const [bgColor, setBgColor] = useState(['#1F2933', '#1F2933', '#1F2933', '#1F2933', '#1F2933', '#1F2933']);
+    const [color, setColor] = useState(['#fff', '#fff', '#fff', '#fff']);
+    const [bgColor, setBgColor] = useState(['#1F2933', '#1F2933', '#1F2933', '#1F2933']);
 
     const onMouseOver = (id) => {
         setColor(color.map((color, index) => index === id - 1 ? '#1F2933' : color));
@@ -19,8 +19,8 @@ export default function Footer() {
     };
 
     const onMouseOut = () => {
-        setColor(['#fff', '#fff', '#fff', '#fff', '#fff', '#fff']);
-        setBgColor(['#1F2933', '#1F2933', '#1F2933', '#1F2933', '#1F2933', '#1F2933']);
+        setColor(['#fff', '#fff', '#fff', '#fff']);
+        setBgColor(['#1F2933', '#1F2933', '#1F2933', '#1F2933']);
     };
 
     return (
@@ -29,33 +29,23 @@ export default function Footer() {
                 <h1 id='contact-me'>CONTACT ME</h1>
                 <Stack direction='horizontal' id='social-media-icons'>
                     <button className='footer-btn' style={{ background: `${bgColor[0]}` }} onMouseOver={() => onMouseOver(1)} onMouseOut={onMouseOut}>
-                        <a href='https://dev.to/petrosdevri' target='_blank' rel='noreferrer'>
-                            <LazyLoad height={35} once><Devto className='social-media-icon' color={color[0]} size='35' title='' /></LazyLoad>
-                       </a>
-                    </button>
-                     <button className='footer-btn' style={{ background: `${bgColor[1]}` }} onMouseOver={() => onMouseOver(2)} onMouseOut={onMouseOut}>
                         <a href='https://github.com/Petrosdevri' target='_blank' rel='noreferrer'>
                             <LazyLoad height={35} once><UilGithub className='social-media-icon' color={color[1]} size='35' /></LazyLoad>
                         </a>
                     </button>
+                    <button className='footer-btn' style={{ background: `${bgColor[1]}` }} onMouseOver={() => onMouseOver(2)} onMouseOut={onMouseOut}>
+                        <a href='https://instagram.com/petrosdevri' target='_blank' rel='noreferrer'>
+                            <LazyLoad height={35} once><UilInstagram className='social-media-icon' color={color[1]} size='35' /></LazyLoad>
+                        </a>
+                    </button>
                     <button className='footer-btn' style={{ background: `${bgColor[2]}` }} onMouseOver={() => onMouseOver(3)} onMouseOut={onMouseOut}>
-                        <a href='https://hashnode.com/@Petrosdevri' target='_blank' rel='noreferrer'>
-                            <LazyLoad height={35} once><Hashnode className='social-media-icon' color={color[2]} size='35' title='' /></LazyLoad>
+                        <a href='https://www.linkedin.com/in/petros-devrikis/' target='_blank' rel='noreferrer'>
+                            <LazyLoad height={35} once><UilLinkedin className='social-media-icon' color={color[2]} size='35' /></LazyLoad>
                         </a>
                     </button>
                     <button className='footer-btn' style={{ background: `${bgColor[3]}` }} onMouseOver={() => onMouseOver(4)} onMouseOut={onMouseOut}>
-                        <a href='https://instagram.com/petrosdevri' target='_blank' rel='noreferrer'>
-                            <LazyLoad height={35} once><UilInstagram className='social-media-icon' color={color[3]} size='35' /></LazyLoad>
-                        </a>
-                    </button>
-                    <button className='footer-btn' style={{ background: `${bgColor[4]}` }} onMouseOver={() => onMouseOver(5)} onMouseOut={onMouseOut}>
-                        <a href='https://www.linkedin.com/in/petros-devrikis/' target='_blank' rel='noreferrer'>
-                            <LazyLoad height={35} once><UilLinkedin className='social-media-icon' color={color[4]} size='35' /></LazyLoad>
-                        </a>
-                    </button>
-                    <button className='footer-btn' style={{ background: `${bgColor[5]}` }} onMouseOver={() => onMouseOver(6)} onMouseOut={onMouseOut}>
                         <a href='mailto:petrosdevrikis@proton.me' target='_blank' rel='noreferrer'>
-                            <LazyLoad height={35} once><ProtonMail className='social-media-icon' color={color[5]} size='32' title='' /></LazyLoad>
+                            <LazyLoad height={35} once><ProtonMail className='social-media-icon' color={color[3]} size='32' title='' /></LazyLoad>
                         </a>
                     </button>
                 </Stack>
