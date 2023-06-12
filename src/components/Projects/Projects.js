066b337858e054ef7be7fdf5717a635ab6ead772
Petros Devrikis <petrosdevri@protonmail.com>
2023-06-13 01:04:37 +0300

    Add 'AcropolisElects' project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Projects.css';
 import ProjectCard from './ProjectCard';
 import { Stack } from 'react-bootstrap';
+import AcropolisElects from '../../images/photos/project-pictures/AcropolisElects.png';
 import ChadCoin from '../../images/photos/project-pictures/ChadCoin.png';
 import FrontendMentorChallenges from '../../images/photos/project-pictures/Frontend Mentor Challenges.png';
 import GreekValues from '../../images/photos/project-pictures/GreekValues.png';
@@ -14,16 +15,17 @@ export default function Projects() {
     return (
         <div className='projects'>
             <Stack className='project-stack' direction='horizontal' gap={5}>
+                <ProjectCard title='AcropolisElects' description='A web application that simulates the distribution of seats in the Greek Parliament based on different electoral systems.' picture={AcropolisElects} code='https://github.com/Petrosdevri/AcropolisElects' demo='https://acropolis-elects.vercel.app/' />
                 <ProjectCard title='ChadCoin' description='A Solidity ERC20 Meme Coin with a JavaScript frontend DApp. Was done for Cadena Web3 course.' picture={ChadCoin} code='https://github.com/Petrosdevri/ChadCoin' demo='https://github.com/Petrosdevri/ChadCoin#readme' />
                 <ProjectCard title='Frontend Mentor Challenges' description='My solutions to Frontend Mentor challenges, using HTML/CSS/JavaScript and Bootstrap.' picture={FrontendMentorChallenges} code='https://github.com/Petrosdevri/Frontend-Mentor-Challenges' demo='https://frontend-mentor-petrosdevri.netlify.app/' />
-                <ProjectCard title='Greek Values' description='The ultimate Greek political quiz, GreekValues, tries to identify popular opinions with Greek ideologies and parties. Built on React.' picture={GreekValues} code='https://github.com/PoliVerse/GreekValues' demo='https://greek-values.vercel.app/' />
             </Stack>
             <Stack className='project-stack' direction='horizontal' gap={5}>
+                <ProjectCard title='Greek Values' description='The ultimate Greek political quiz, GreekValues, tries to identify popular opinions with Greek ideologies and parties. Built on React.' picture={GreekValues} code='https://github.com/PoliVerse/GreekValues' demo='https://greek-values.vercel.app/' />
                 <ProjectCard title='Greek VAT Calculator' description='An HTML/CSS/JavaScript website in which users can calculate VAT rates in Greece by inserting their costs.' picture={GreekVATCalculator} code='https://github.com/Petrosdevri/Greek-VAT-Calculator' demo='https://greek-vat-calculator.vercel.app/' />
                 <ProjectCard title='Jason Portfolio' description='A single-page portfolio website built on React for a friend of mine.' picture={JasonPortfolio} code='https://github.com/Petrosdevri/JasonPortfolio' demo='https://jasonkehagias.netlify.app/' />
-                <ProjectCard title='Math Training Game' description='A training game built on HTML/CSS/JavaScript developed to help people on math operations.' picture={MathTrainingGame} code='https://github.com/Petrosdevri/Math-Training-Game' demo='https://petrosdevri-math-game.netlify.app/' />
             </Stack>
             <Stack className='project-stack' direction='horizontal' gap={5}>
+                <ProjectCard title='Math Training Game' description='A training game built on HTML/CSS/JavaScript developed to help people on math operations.' picture={MathTrainingGame} code='https://github.com/Petrosdevri/Math-Training-Game' demo='https://petrosdevri-math-game.netlify.app/' />
                 <ProjectCard title='Product CRUD App' description='An Electron and MySQL sample app. Users can submit inputs about a product which are stored at a database and displayed at the interface.' picture='' code='https://github.com/Petrosdevri/Product-CRUD-App' demo='https://github.com/Petrosdevri/Product-CRUD-App' />
                 <ProjectCard title='React Quiz' description='A sample quiz built on React featuring questions about Greece.' picture={ReactQuiz} code='https://github.com/Petrosdevri/React-Greece-Quiz' demo='https://react-greece-quiz.netlify.app/' />
             </Stack>
