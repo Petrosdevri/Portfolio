91851bcc37728ff0eaae8b219dc829eeff7aeffe
Petros Devrikis <100411628+Petrosdevri@users.noreply.github.com>
2024-02-27 18:22:35 +0000

    Worked on"'Contact" page

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,12 +1,31 @@
-import React from 'react';
-import { Container } from 'react-bootstrap';
+import React, { useRef } from 'react';
+import { Button, Container, Stack } from 'react-bootstrap';
 import './Contact.css';
 
 export default function Contact() {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        // emailjs.sendForm('SERVICE_ID', 'TEMPLATE_ID', form.current, {
+        //     publicKey: 'PUBLIC_KEY',
+        // }).then(() => alert('SUCCESS!'))
+        // .catch(() => console.log('FAILED!'));
+    }
+
     return (
         <Container fluid className='contact'>
             <h1>Contact <span className='name'>Me</span></h1>
-            <p>This section is coming soon.</p>
+            <h3>This feature is still in experimental phase so sending emails is disabled.</h3>
+            <form className='contact-form' ref={form} onSubmit={sendEmail}>
+                <Stack gap={4}>
+                    <input type='email' className='email field' placeholder='Email Address' />
+                    <input type='text' className='subject field' placeholder='Subject' />
+                    <textarea className='message field' rows='10' placeholder='Enter your message...'></textarea>
+                    <Button type='submit' disabled>Submit</Button>
+                </Stack>
+            </form>
         </Container>
     );
 }
