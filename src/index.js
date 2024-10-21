5cab67227575ad56bcfa85d65e634b39181f0e8a
Petros Devrikis <100411628+Petrosdevri@users.noreply.github.com>
2024-10-21 08:18:37 +0000

    Added "Web Scrapers" project

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React, { Profiler } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/react-bootstrap/dist/react-bootstrap.min.js';
 
@@ -12,11 +11,6 @@ root.render(
     <App />
   </Profiler>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
 
 /* <React.StrictMode>
       <App />
